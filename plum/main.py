504a3ec84298f4e7435b85da1f8622d6ef676ae8
Jeff Shen <shenjeff@princeton.edu>
2023-06-24 22:56:11 -0400

    initial implementation

---
+++ plum/main.py
@@ -0,0 +1,45 @@
+import subprocess
+
+
+def main():
+    jobname = input("Job name: ")
+    nodes = input("Nodes (default 1): ") or 1
+    tasks = input("Tasks (default 1): ") or 1
+    cpus_per_task = input("CPUs per task (default 1): ") or 1
+    gpus = input("GPUs (default 1): ") or 1
+    mem_per_cpu = input("Memory per CPU (default 10G): ") or "10G"
+    time = input("Time (default 00:10:00): ") or "00:10:00"
+    email = input("Email (default js5013@princeton.edu): ") or "js5013@princeton.edu"
+    command = input("Command: ")
+
+    batch_file = f"""
+#!/bin/bash
+#SBATCH --job-name={jobname}
+#SBATCH --output={jobname}.out
+#SBATCH --error={jobname}.err
+#SBATCH --nodes={nodes}
+#SBATCH --ntasks={tasks}
+#SBATCH --cpus-per-task={cpus_per_task}
+#SBATCH --gres=gpu:{gpus}
+#SBATCH --mem-per-cpu={mem_per_cpu}
+#SBATCH --time={time}
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-user={email}
+
+{command}
+    """
+
+    run = input("Run? (Y/n): ") or "y"
+
+    if run.lower() == "n":
+        print(batch_file)
+    elif run.lower() == "y":
+        with open(f"{jobname}.slurm", "w") as f:
+            f.write(batch_file)
+        subprocess.run(["sbatch", f"{jobname}.slurm"])
+        subprocess.run(["rm", f"{jobname}.slurm"])
+
+
+if __name__ == "__main__":
+    main()
