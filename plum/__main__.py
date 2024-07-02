4f40d9344017b88eff1d0d16b0faeaa48f35f9bb
Jeff Shen <shenjeff@princeton.edu>
2024-07-02 00:36:30 -0400

    more flexible args; save file; no default email

--- plum/__main__.py
+++ plum/__main__.py
@@ -2,46 +2,56 @@ import subprocess
 
 
 def main():
-    print("Hello! My name is Slurpy :) \nI'd love to help you with your slurm job. Can you please give me the following info \nand I'll write your file for you! <3")
-    
-    jobname = input("Job name: ")
+    print(
+        "Hello! My name is Slurpy :) I'd love to help you with your Slurm job. Can you please give me the following info and I'll write your file for you! <3"
+    )
+
+    jobname = input('Job name (default "slurpy"): ') or "slurpy"
     nodes = input("Nodes (default 1): ") or 1
     tasks = input("Tasks (default 1): ") or 1
     cpus_per_task = input("CPUs per task (default 1): ") or 1
     gpus = input("GPUs (default 1): ") or 1
+    constraint = input('Constraint (default "a100"): ') or "a100"
+    partition = input("Partition (default none): ") or None
     mem_per_cpu = input("Memory per CPU (default 10G): ") or "10G"
     time = input("Time (default 00:10:00): ") or "00:10:00"
-    email = input("Email (default js5013@princeton.edu): ") or "js5013@princeton.edu"
+    email = input("Email (default none): ") or None
     command = input("Command: ")
+    save = input("Save file? (Y/n): ") or "y"
+    run = input("Run? (Y/n): ") or "y"
 
     batch_file = f"""#!/bin/bash
 #SBATCH --job-name={jobname}
 #SBATCH --output={jobname}.out
 #SBATCH --error={jobname}.err
 #SBATCH --nodes={nodes}
+#SBATCH --constrant={constraint}
 #SBATCH --ntasks={tasks}
 #SBATCH --cpus-per-task={cpus_per_task}
 """
     if int(gpus) > 0:
         batch_file += f"""\n#SBATCH --gres=gpu:{gpus}"""
+    if partition is not None:
+        batch_file += f"""\n#SBATCH --partition={partition}"""
 
     batch_file += f"""\n#SBATCH --mem-per-cpu={mem_per_cpu}
-#SBATCH --time={time}
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
-#SBATCH --mail-user={email}
+#SBATCH --time={time}"""
 
-{command}
-    """
+    if email is not None:
+        batch_file += f"""\n#SBATCH --mail-type=ALL
+#SBATCH --mail-user={email}"""
 
-    run = input("Run? (Y/n): ") or "y"
+    batch_file += f"""\n\n{command}"""
 
-    if run.lower() == "n":
+    with open(f"{jobname}.slurm", "w") as f:
+        f.write(batch_file)
+
+    if run.lower() in ["n", "no"]:
         print(batch_file)
-    elif run.lower() == "y":
-        with open(f"{jobname}.slurm", "w") as f:
-            f.write(batch_file)
+    elif run.lower() in ["y", "yes"]:
         subprocess.run(["sbatch", f"{jobname}.slurm"])
+
+    if save.lower() in ["n", "no"]:
         subprocess.run(["rm", f"{jobname}.slurm"])
 
 
