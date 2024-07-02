82e0cea6002d11740aac1642965eca4060cc53dd
Jeff Shen <shenjeff@princeton.edu>
2024-07-02 00:41:25 -0400

    optional constraint; default 0 gpus

--- plum/__main__.py
+++ plum/__main__.py
@@ -10,8 +10,8 @@ def main():
     nodes = input("Nodes (default 1): ") or 1
     tasks = input("Tasks (default 1): ") or 1
     cpus_per_task = input("CPUs per task (default 1): ") or 1
-    gpus = input("GPUs (default 1): ") or 1
-    constraint = input('Constraint (default "a100"): ') or "a100"
+    gpus = input("GPUs (default 0): ") or 0
+    constraint = input("Constraint (default none): ") or None
     partition = input("Partition (default none): ") or None
     mem_per_cpu = input("Memory per CPU (default 10G): ") or "10G"
     time = input("Time (default 00:10:00): ") or "00:10:00"
@@ -25,12 +25,13 @@ def main():
 #SBATCH --output={jobname}.out
 #SBATCH --error={jobname}.err
 #SBATCH --nodes={nodes}
-#SBATCH --constrant={constraint}
 #SBATCH --ntasks={tasks}
 #SBATCH --cpus-per-task={cpus_per_task}
 """
     if int(gpus) > 0:
         batch_file += f"""\n#SBATCH --gres=gpu:{gpus}"""
+    if constraint is not None:
+        batch_file += f"""\n#SBATCH --constraint={constraint}"""
     if partition is not None:
         batch_file += f"""\n#SBATCH --partition={partition}"""
 
